9fcbc26c0b8b442f5784eb368b9bd200d51f9027
Daniel Nagy <danielnagy@posteo.de>
2025-12-14 20:30:00 +0100

    Add logging

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
+use log::{debug, error, info};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::collections::HashMap;
@@ -20,7 +21,6 @@ static GLOBAL_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 static GLOBAL_MAP: LazyLock<Mutex<HashMap<usize, TcpStream>>> = LazyLock::new(|| {
     let map = HashMap::new();
-    // map.insert("initial_key".to_string(), 100);
     Mutex::new(map)
 });
 
@@ -57,12 +57,6 @@ fn connect_nc_command(stream: TcpStream, args: &[serde_json::Value]) {
     });
 }
 
-// fn insert_global_stream(tcp: TcpStream) -> usize {
-//     let current_count = GLOBAL_COUNTER.fetch_add(1, Ordering::SeqCst);
-//     GLOBAL_MAP.lock().unwrap().insert(current_count, tcp);
-//     current_count
-// }
-
 #[derive(Debug, Deserialize)]
 pub struct JsonRPCRequest {
     pub id: i32,
@@ -78,22 +72,21 @@ pub struct JsonRPCResponse {
 }
 
 fn main() {
+    colog::init();
     std::thread::spawn(|| {
-        server_thread();
+        if let Err(err) = server_thread() {
+            error!("{:?}", err);
+            std::process::exit(1);
+        }
     });
     for line in std::io::stdin().lock().lines() {
         let line = line.expect("Error reading line.");
-        // Skip empty lines
         if line.trim().is_empty() {
             continue;
         }
         let request: JsonRPCRequest =
             serde_json::from_str(&line).expect("Failed to parse line as JSON");
-        // if parsed_json.get("jsonrpc") != Some(&Value::String("2.0".to_string())) {
-        //     println!("  -> invalid JSON-RPC 2.0 message");
-        //     break;
-        // }
-        eprintln!("  -> {:?}", request);
+        debug!("  -> {:?}", request);
         let result: serde_json::Value = match (request.method.as_str(), request.params.as_slice()) {
             ("add", numbers) => {
                 let mut ret = 0u64;
@@ -106,20 +99,15 @@ fn main() {
                 let num = number.as_u64().unwrap() as usize;
                 let mut lock = GLOBAL_MAP.lock().unwrap();
                 let mut stream = lock.remove(&num).unwrap();
-                eprintln!("testing122");
                 stream.write_all(b"HTTP/1.0 200 OK\r\n\r\n").unwrap();
-                eprintln!("testing123");
                 connect_nc_command(stream, args);
-                eprintln!("testing126");
                 "accepted".into()
             }
             ("deny", [number, _args @ ..]) => {
                 let num = number.as_u64().unwrap() as usize;
                 let mut lock = GLOBAL_MAP.lock().unwrap();
                 let mut stream = lock.remove(&num).unwrap();
-                eprintln!("testing127");
                 stream.write_all(b"HTTP/1.0 403 Denied\r\n\r\n").unwrap();
-                eprintln!("testing128");
                 "denied".into()
             }
             ("concat", words) => {
@@ -140,65 +128,53 @@ fn main() {
             id: request.id,
             result: result.into(),
         };
-        let mut lock = std::io::stdout().lock();
-        writeln!(lock, "{}", serde_json::to_string(&response).unwrap()).unwrap();
-        lock.flush().unwrap();
-        drop(lock);
+        println!("{}", serde_json::to_string(&response).unwrap());
+        std::io::stdout().flush().unwrap();
     }
 }
 
-fn server_thread() {
+fn server_thread() -> Result<(), Box<dyn std::error::Error>> {
     let port: u16 = 3128;
-    let listener =
-        TcpListener::bind(&format!("127.0.0.1:{}", port)).expect("Failed to bind to address");
-    eprintln!("Server listening on 127.0.0.1:{}", port);
+    let listener = TcpListener::bind(&format!("127.0.0.1:{}", port))?;
+    info!("Server listening on 127.0.0.1:{}", port);
     for stream in listener.incoming() {
         let stream = stream.expect("Connection failed");
         thread::spawn(move || {
             handle_connection(stream);
         });
     }
+    return Err("Should not arrive here.".into());
 }
 
 pub fn handle_connection(mut stream: TcpStream) {
-    // use base64::{Engine as _, engine::general_purpose};
     let current_count = GLOBAL_COUNTER.fetch_add(1, Ordering::SeqCst);
-    eprintln!("New connection established!: {}", current_count);
+    info!("New connection established number: {}", current_count);
     let mut buffer = [0; 1024];
     while let Ok(size) = stream.read(&mut buffer) {
         if size == 0 {
             break;
         }
-        let buffer_string = String::from_utf8_lossy(&buffer);
-        let lines: Vec<&str> = buffer_string.lines().collect();
-        let mut hostport_pair: &str = lines[0].split(" ").collect::<Vec<&str>>()[1];
+        let mut headers = [httparse::EMPTY_HEADER; 16];
+        let mut req = httparse::Request::new(&mut headers);
+        req.parse(&buffer).unwrap();
+        let mut hostport_pair = req.path.unwrap();
         if hostport_pair.starts_with("https://") {
             hostport_pair = hostport_pair.strip_prefix("https://").unwrap();
         }
         let (host, port) = hostport_pair.split_once(":").unwrap();
         let port: u16 = port.parse().unwrap();
-        // let encoded_string: String = general_purpose::STANDARD.encode(&buffer[..size]);
         let value = json!({
             "jsonrpc": "2.0",
             "method": "want",
             "params": [
-                // encoded_string,
                 host,
                 port,
                 current_count,
             ]
         });
-        let mut lock = std::io::stdout().lock();
-        writeln!(lock, "{}", serde_json::to_string(&value).unwrap()).unwrap();
-        lock.flush().unwrap();
-        drop(lock);
+        println!("{}", serde_json::to_string(&value).unwrap());
+        std::io::stdout().flush().unwrap();
         GLOBAL_MAP.lock().unwrap().insert(current_count, stream);
         break;
-        // println!("Received: {}", request);
-        // if stream.write_all(&buffer[..size]).is_err() || stream.flush().is_err() {
-        //     eprintln!("Failed to write to stream");
-        //     break;
-        // }
     }
-    // eprintln!("Connection closed.");
 }
