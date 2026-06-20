972c53eca5793494f8a274ff218cc40d131936bd
Daniel Nagy <danielnagy@posteo.de>
2026-06-20 23:45:00 +0200

    Add LICENSE file

--- default.nix
+++ default.nix
@@ -8,7 +8,15 @@ rustPlatform.buildRustPackage (finalAttrs: {
   pname = "jsonrpc-httpproxy";
   version = "0.0.1";
   src = lib.cleanSource ./.;
+
   cargoLock = {
     lockFile = ./Cargo.lock;
+  };
+
+  meta = with lib; {
+    license = licenses.agpl3Plus;
+    maintainers = with lib.maintainers; [ nagy ];
+    platforms = platforms.linux;
+    mainProgram = "jsonrpc-httpproxy";
   };
 })
